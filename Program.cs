b544e8404521bacd7ad5be296511d2cbc06a0808
William Cowell <williamcowell@Williams-MBP.lan>
2020-09-18 19:08:32 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using OptimizelySDK;
+using OptimizelySDK.Entity;
+using System.Collections.Generic;
+
+namespace optimizely_test
+{
+    class Program
+    {
+        /// <param name="sdkKey">Optimizely SDK key.</param>
+        /// <param name="feature">Name of the feature you want to check.</param>
+        /// <param name="iterations">Number of iterations to test the feature.</param>
+        /// <param name="userId">Value that uniquely identifies the user.</param>
+        /// <param name="attributes">Colon-delimited key-value pairs separated by spaces.</param>
+        static void Main(string sdkKey, string feature, string userId = null, int iterations = 1, params string[] attributes)
+        {
+            CheckOptimizelyFeature(sdkKey, feature, userId, iterations, ExtractAttributes(attributes));
+        }
+
+        private static IEnumerable<(string, string)> ExtractAttributes(params string[] attributes)
+            => attributes
+                .Select(attribute => attribute.Split(new[] { ':', '=' }))
+                .Select(attribute => (attribute.ElementAtOrDefault(0), attribute.ElementAtOrDefault(1)));
+
+        private static void CheckOptimizelyFeature(string sdkKey, string feature, string userId, int iterations, IEnumerable<(string, string)> attributes)
+        {
+            var userAttributes = new UserAttributes();
+
+            Console.WriteLine("SDK Key:\t{0}", sdkKey);
+            Console.WriteLine("Feature:\t{0}", feature);
+            Console.WriteLine("Iterations:\t{0}", iterations);
+            Console.WriteLine("Attributes:");
+
+            foreach (var (attribute, value) in attributes)
+            {
+                if(userAttributes.TryAdd(attribute, value))
+                    Console.WriteLine("\t{0}:\t{1}", attribute, value);
+            }
+
+            var isEnabledCount = 0d;
+
+            Console.Write("IsEnabled:\n\t");
+
+            using (var optimizelyInstance = OptimizelyFactory.NewDefaultInstance(sdkKey))
+            for (int i = 0; i < iterations; i++)
+            {
+                var isEnabled = optimizelyInstance.IsFeatureEnabled(
+                    feature, userId ?? Guid.NewGuid().ToString(), userAttributes);
+                Console.Write("{0}", isEnabled ? '*' : '.');
+
+                if (isEnabled)
+                    ++isEnabledCount;
+            }
+
+            Console.WriteLine("\n\t{0}/{1} ({2}%)", isEnabledCount, iterations, 100 * isEnabledCount / iterations);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Optimizely-Test
+
+This app will check the effective rollout percentage for a given set of user attributes.
+
+## How to build
+
+```bash
+dotnet build
+```
+
+## Usage
+
+```bash
+./optimizely-test -h
+./optimizely-test
+    --sdk-key [SDK key]
+    --feature [name of feature]
+    --iterations [number of iterations]
+    --attributes [colon-delimited, space separated user attributes]
+```
+
+## Example
+
+```bash
+./optimizely-test --sdk-key DUM4Asnd3C8G7Befbq5xb --feature my_cool_feature --iterations 1000 --attributes hostname:beta.mysite.com ip_address:84.64.145.96 device:ios
+```
